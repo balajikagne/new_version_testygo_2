c37699a78b25ba2f1ff87d3e0c33ead70cf661f6
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2023-10-13 11:53:57 +0530

    Update server.js new
    changes something

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ mongoose.connect(DB,{useUnifiedTopology:true,useNewUrlParser:true}).then(()=>{
 
 //
 app.use(cors({
-    origin:["https://deploy-mern-1whq.vercel.app"],
+    origin:["https://new-version-testygo-2-api.vercel.app/"],
     methods:["POST","GET"],
     Credential:true
 }))
